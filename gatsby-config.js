dfbb9abfb48a74d72ce8e6906f0dcf8787478c1c
ymmmtym <26668352+ymmmtym@users.noreply.github.com>
2021-08-29 14:34:15 +0900

    add code-title

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,10 +24,15 @@ module.exports = {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
+          {
+            resolve: `gatsby-remark-code-titles`,
+          },
           'gatsby-remark-autolink-headers',
+          // `gatsby-remark-code-titles`,
           {
             resolve: 'gatsby-remark-prismjs',
             options: {
+              classPrefix: "language-",
               showLineNumbers: true,
             },
           },
